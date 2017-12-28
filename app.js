00302ea068ac7a7d515ab3e06ec72ed5d610e9a6
tanishq1g <tanishq.gupta@iiitb.org>
2017-12-28 14:39:33 +0530

    index, new, create, show route routes working and show view added

--- app.js
+++ app.js
@@ -1,34 +1,40 @@
 var express = require('express');
 var app = express();
+
+
 //connecting to database
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://localhost/yelp_camp");
 
+
 //setting up all rendered files to be ejs files
 app.set('view engine','ejs');
+
 
 //for post routes
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+
 //SCHEMA setup
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
-
 var Campground = mongoose.model('Campground', campgroundSchema);
 
 
-//adds to database
+// adds to database
 // Campground.create(
 //     // {
 //     //     name: 'Bheemeshwari Jungle Lodges – Karnataka',
 //     //     image: 'https://www.holidify.com/blog/wp-content/uploads/2016/08/bheemeshwari-jungle-lodge.jpg'
 //     // },
 //     {
-//         name : 'salman',
-//         image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg"
+//         name : 'Tsomoriri',
+//         image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg",
+//         description: 'this is a huge green hill. beautiful, scenic'
 //     },
 //     function(err, campground){
 //         if(err){
@@ -39,18 +45,19 @@ var Campground = mongoose.model('Campground', campgroundSchema);
 //             console.log(campground);
 //         }
 //     }
-// )
+// );
 // var campgrounds = [
 //     {name : 'salman',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg"},
 //     {name : 'granite',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-exotica.jpg"},
 //     {name : 'bolomon',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-room-on-the-roof.jpg"}
 // ];
 
-
+//landing page
 app.get('/',function(req,res){
     res.render("landing");
 });
 
+//INDEX - GET - /dogs - displays list of all campgrounds
 app.get('/campgrounds',function(req,res){
     // res.render('campgrounds',{campgrounds: campgrounds});
     //retrieve campgroudns from the database
@@ -60,32 +67,51 @@ app.get('/campgrounds',function(req,res){
         }
         else{
             //redirect to campgrounds page
-            res.render('campgrounds',{campgrounds: allcampgrounds});
+            res.render('index',{campgrounds: allcampgrounds});
         }
     });
 });
 
+//CREATE - POST - /dogs -add conent to db
 app.post('/campgrounds', function(req,res){
     var name = req.body.name;
     var image = req.body.image;
-
-    var newCampground = {name: name, image: image};
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description : desc};
     //create a new campground and save to database
     Campground.create(newCampground, function(err, newlyCreated){
         if(err){
             console.log(err);
         }
         else{
-            res.redirect('campgrounds');
+            res.redirect('index');
         }
     });
     // campgrounds.push({name: name, image: image});
     // res.redirect('campgrounds');
 });
-// to show the form that will call the post route
+
+//NEW - GET - /dogs/new - to show the form that will call the post route
 app.get('/campgrounds/new',function(req,res){
     res.render('new');
 });
+
+//SHOW - GET - /dogs/:id - shows info about 1 dog
+app.get('/campgrounds/:id',function(req,res){
+    //find the campground with provided id and then show the prticular campground
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+
+            res.render('show',{campground: foundCampground});
+        }
+    });
+});
+
+
+
 app.listen(8081, (req, res) => {
     console.log("listening...");
 });

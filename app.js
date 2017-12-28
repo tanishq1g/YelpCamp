12d6041b65ee8b90463cef1fbe29ccb1adbcffef
tanishq1g <tanishq.gupta@iiitb.org>
2017-12-28 07:49:38 +0530

    mongodb basic setup, mongoose added

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 var express = require('express');
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/cat_app");
+
+//setting up all rendered files to be ejs files
 app.set('view engine','ejs');
 
+//for post routes
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+//SCHEMA setup
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
+var Campground = mongoose.model('Campground', campgroundSchema);
+
+// Campground.create(
+//     {
+//         name: 'Bheemeshwari Jungle Lodges – Karnataka',
+//         image: 'https://www.holidify.com/blog/wp-content/uploads/2016/08/bheemeshwari-jungle-lodge.jpg'
+//     },
+//     function(err, campground){
+//
+//     }
+// )
 var campgrounds = [
     {name : 'salman',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg"},
     {name : 'granite',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-exotica.jpg"},
     {name : 'bolomon',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-room-on-the-roof.jpg"}
 ];
+
+
 app.get('/',function(req,res){
     res.render("landing");
 });

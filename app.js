7b4703b94b31d7dd2bb7926285d5bef3d36b4f66
tanishq1g <tanishq.gupta@iiitb.org>
2017-12-28 13:25:23 +0530

    post and get routes for campground modified according to mongoose

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
-
+//connecting to database
 var mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost/cat_app");
+mongoose.connect("mongodb://localhost/yelp_camp");
 
 //setting up all rendered files to be ejs files
 app.set('view engine','ejs');
@@ -19,20 +19,32 @@ var campgroundSchema = new mongoose.Schema({
 
 var Campground = mongoose.model('Campground', campgroundSchema);
 
+
+//adds to database
 // Campground.create(
+//     // {
+//     //     name: 'Bheemeshwari Jungle Lodges – Karnataka',
+//     //     image: 'https://www.holidify.com/blog/wp-content/uploads/2016/08/bheemeshwari-jungle-lodge.jpg'
+//     // },
 //     {
-//         name: 'Bheemeshwari Jungle Lodges – Karnataka',
-//         image: 'https://www.holidify.com/blog/wp-content/uploads/2016/08/bheemeshwari-jungle-lodge.jpg'
+//         name : 'salman',
+//         image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg"
 //     },
 //     function(err, campground){
-//
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             console.log("newly created campground");
+//             console.log(campground);
+//         }
 //     }
 // )
-var campgrounds = [
-    {name : 'salman',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg"},
-    {name : 'granite',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-exotica.jpg"},
-    {name : 'bolomon',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-room-on-the-roof.jpg"}
-];
+// var campgrounds = [
+//     {name : 'salman',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/Tsomoriri.jpg"},
+//     {name : 'granite',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-exotica.jpg"},
+//     {name : 'bolomon',image : "https://www.holidify.com/blog/wp-content/uploads/2016/08/camp-room-on-the-roof.jpg"}
+// ];
 
 
 app.get('/',function(req,res){
@@ -40,16 +52,35 @@ app.get('/',function(req,res){
 });
 
 app.get('/campgrounds',function(req,res){
-    res.render('campgrounds',{campgrounds: campgrounds});
+    // res.render('campgrounds',{campgrounds: campgrounds});
+    //retrieve campgroudns from the database
+    Campground.find(function(err,allcampgrounds){
+        if(err){
+            console.log(err);
+        }
+        else{
+            //redirect to campgrounds page
+            res.render('campgrounds',{campgrounds: allcampgrounds});
+        }
+    });
 });
 
 app.post('/campgrounds', function(req,res){
     var name = req.body.name;
     var image = req.body.image;
 
-    campgrounds.push({name: name, image: image});
-    // res.send('post route');
-    res.redirect('campgrounds');
+    var newCampground = {name: name, image: image};
+    //create a new campground and save to database
+    Campground.create(newCampground, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.redirect('campgrounds');
+        }
+    });
+    // campgrounds.push({name: name, image: image});
+    // res.redirect('campgrounds');
 });
 // to show the form that will call the post route
 app.get('/campgrounds/new',function(req,res){

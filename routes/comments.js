f6fca315cdbd96b787c85fdbf60aa83dc097e076
tanishq1g <tanishq.gupta@iiitb.org>
2018-03-18 11:08:00 +0530

    edit routes and views added for comments

--- routes/comments.js
+++ routes/comments.js
@@ -23,6 +23,41 @@ router.get('/new',isLoggedIn, function(req,res){
     });
 });
 
+router.get('/:comment_id/edit',function(req,res){
+    console.log('edit route for comment',req.params);
+    Campground.findById(req.params.id, function(err,foundCampground){
+        if (err) {
+            console.log("error finding campground",err)
+        }
+        else {
+            console.log(foundCampground);
+            Comment.findById(req.params.comment_id, function(err,foundComment){
+                if (err) {
+                    console.log("error finding campground",err)
+                    res.redirect("back")
+                }
+                else {
+                    console.log(foundCampground,foundComment);
+                    res.render('comments/edit',{
+                        campground: foundCampground,
+                        comment : foundComment
+                    });
+                }
+            });
+        }
+    });
+})
+
+router.put('/:comment_id',function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment) {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+})
+
 
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
